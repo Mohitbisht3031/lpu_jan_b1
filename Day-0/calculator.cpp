e76c7bb30abb489baa831e51efdfd2ee79e9619d
Mohit <59963561+Mohitbisht3031@users.noreply.github.com>
2022-01-18 18:25:10 +0530

    Add files via upload

---
+++ Day-0/calculator.cpp
@@ -0,0 +1,19 @@
+// #include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int sum(int n1,int n2){
+    return n1+n2;
+}
+
+int main(){
+
+  // int,float,char,string,long long,double,short
+  int n1,n2;
+  cin>>n1>>n2;
+
+  cout<<"Sum of n1 & n2 is : "<<sum(n1,n2);
+
+  return 0;
+}
---
+++ Day-0/hello.cpp
@@ -0,0 +1,13 @@
+// #include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+  cout<<"Hello word.!";
+  cout<<"helloo all.!";
+
+
+  return 0;
+}
